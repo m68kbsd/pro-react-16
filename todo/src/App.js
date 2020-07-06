38a4873ae83811975e7ab30e205423f9a8c26b85
m68kbsd <markmbirira@gmail.com>
2020-07-06 03:46:47 +0300

    Ch01 - Complete ToDo app

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import { TodoBanner } from './TodoBanner';
+import { TodoCreator } from './TodoCreator';
+import { TodoRow } from './TodoRow';
+import { VisibilityControl } from './VisibilityControl';
 
 class App extends Component {
 
@@ -14,60 +16,68 @@ class App extends Component {
                   { action: "Get Sample thinkpads for sale", done: false},
                   { action: "Integrate Payments and Shipping methods to Clients", done: false},
                   { action: "Host application on Vultr OpenBSD", done: true}],
-      newItemText: ""
+      showCompleted: true
     }
   }
 
-  updateNewTextValue = (event) => {
-    this.setState({newItemText: event.target.value});
-  }
-
-  createNewTodo = () => {
-    if (!this.state.todoItems
-          .find(item => item.action === this.state.newItemText)) {
-      this.setState({
-        todoItems: [...this.state.todoItems,
-          {action: this.state.newItemText, done: false}],
-        newItemText: ""
-      });
+  createNewTodo = (task) => {
+    if (!this.state.todoItems.find(item => item.action === task)) {
+        this.setState({
+          todoItems: [...this.state.todoItems, { action: task, done: false }]
+        }, () => localStorage.setItem("todos", JSON.stringify(this.state)));
     }
   }
 
   toggleTodo = (todo) => this.setState({
     todoItems:
       this.state.todoItems.map(item => item.action === todo.action
-        ? { ...item, done: !item.done } : item)
-  })
+        ? { ...item, done: !item.done } : item) });
 
-  todoTableRows = () => this.state.todoItems.map(item =>
-    <tr key={item.action}>
-      <td>{item.action}</td>
-      <td>
-        <input type="checkbox" checked={item.done}
-          onChange={() => this.toggleTodo(item)} />
-      </td>
-    </tr>);       
+  todoTableRows = (doneValue) => this.state.todoItems
+        .filter(item => item.done === doneValue)
+        .map(item =>
+          <TodoRow key={ item.action } item={ item } callback={ this.toggleTodo } />);       
+  
+  componentDidMount = () => {
+    let data = localStorage.getItem("todos");
+    this.setState(data != null 
+      ? JSON.parse(data)
+      : {
+          userName: "Thinkpad Center",
+          todoItems: [{ action: "Buy domain name", done: false},
+                      { action: "Develop business application", done: false},
+                      { action: "Deploy on Vultr", done: false}],
+          showCompleted: true                      
+      });
+  }
 
   render = () => 
-    <div className="App">
-      <h4 className="bg-primary text-white text-center p-2">
-        {this.state.userName}'s To-Do List
-        ({this.state.todoItems.filter(t => !t.done).length} items to do)
-      </h4>
+    <div>
+      <TodoBanner name={this.state.userName} tasks={ this.state.todoItems } />
       <div className="container-fluid">
-        <div className="my-1">
-          <input className="form-control"
-            value={this.state.newItemText}
-            onChange={this.updateNewTextValue} />
-          <button className="btn btn-primary mt-1"
-            onClick={this.createNewTodo}>Add</button>
-        </div>
+        <TodoCreator callback={ this.createNewTodo } />
         <table className="table table-stripped table-bordered">
           <thead>
             <tr><th>Description</th><th>Done</th></tr>
           </thead>
-          <tbody>{this.todoTableRows()}</tbody>
+          <tbody>{this.todoTableRows(false)}</tbody>
         </table>
+        <div className="bg-secondary text-white text-center p-2">
+            <VisibilityControl description="Completed Tasks"
+              isChecked={this.state.showCompleted}
+              callback={ (checked) => this.setState({ showCompleted: checked } )} />
+        </div>
+
+        { this.state.showCompleted && 
+            <table className="table table-stripped table-bordered">
+              <thead>
+                <tr><th>Description</th><th>Done</th></tr>
+              </thead>
+              <tbody>
+                {this.todoTableRows(true)}
+              </tbody>
+            </table>
+        }
       </div>
     </div>
 }
